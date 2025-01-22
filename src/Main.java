e923ef21161357484d214acc2f46e38501d1fd37
temesgentesfazgi <natitesfazgi@gmail.com>
2025-01-22 15:08:51 -0800

    Used scanner to read inputs

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello idea! Let's get out of here early!");
-//        for (int i = 1; i <= 5; i++) {
-//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-//            System.out.println("i = " + i);
-//        }
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hi " + name);
+    }//end constructor
+}//end main
